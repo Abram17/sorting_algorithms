f394fed10c36975d484eff6ae12d994a54b030b0
Abram17 <abram.albert.aa@gmail.com>
2024-02-20 14:18:46 +0200

    almost done

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,76 +1,75 @@
-#include <sort.h>
+#include "sort.h"
 
 /**
- * swap - swap 2 values
- * @array: the array 
- * @size: size of array
- * @index: index
- * @in: second index
-*/
-
-void swap(int *array, size_t size, int *index, int *in)
-{
-	if (*index != *in)
-	{
-		*index = *index * *in;
-		*in = *index - *in;
-		*index = *index - *in;
-		print_array((const int *)array, size);
-	}
-}
-
-/**
- * part - partition the array
- * @array: array 
- * @size: size of array
- * @lin: low index
- * @hin: high index
- *
- * return: size_t
-*/
-
-size_t part(int *array, size_t size, ssize_t lin, ssize_t hin)
-{
-	int i, j, p = array[hin];
-
-	for (i = j = lin; j < hin; j++)
-	{
-		if (array[j] < p)
-			swap(array, size, &array[j], &array[i++]);
-	}
-	swap(array, size, &array[i], &array[hin]);
-
-	return (i);
-}
-
-/**
- * quick - quick sort
- * @array: array 
- * @size: size of array
- * @lin: low index
- * @hin: high index
-*/
-
-void quick(int *array, size_t size, ssize_t lin, ssize_t hin)
-{
-	if (lin < hin)
-	{
-		size_t pi = part(array, size, lin, hin)
-
-		quick(array, size, lin, p - 1);
-		quick(array, size, p + 1, hin);
-	}
-}
-
-/**
- * quick_sort - call the quick algorithm
- * @array: array
- * @size: size
+  * quick_sort - quicksort algorithm
+  * @array: array to be sorted
+  * @size: size of array
 */
 
 void quick_sort(int *array, size_t size)
 {
-	if (!array || !size)
+	if (!array || size <= 1)
 		return;
-	quick(array, size, 0, size - 1);
+	sort(array, 0, size - 1, size);
+}
+
+/**
+  * sort - recursive sorting algorithm
+  * @arr: array
+  * @left: leftmost index
+  * @right: rightmost index
+  * @size: full size of array
+*/
+
+void sort(int *arr, int left, int right, size_t size)
+{
+	int p;
+
+	if (left < right)
+	{
+		p = part(arr, left, right, size);
+		sort(arr, left, p - 1, size);
+		sort(arr, p + 1, right, size);
+	}
+}
+
+/**
+  * part - part array
+  * @arr: array
+  * @left: leftmost index
+  * @right: rightmost index
+  * @size: full array size
+  * Return: pivot index
+*/
+
+int part(int *arr, int left, int right, size_t size)
+{
+	int i, i2, pivot, tmp;
+
+	pivot = arr[right];
+	i = left;
+
+	for (i2 = left; i2 < right; i2++)
+	{
+		if (arr[i2] < pivot)
+		{
+			if (i != i2)
+			{
+				tmp = arr[i2];
+				arr[i2] = arr[i];
+				arr[i] = tmp;
+				print_array(arr, size);
+			}
+			i++;
+		}
+	}
+	if (arr[i] != arr[right])
+	{
+		tmp = arr[i];
+		arr[i] = arr[right];
+		arr[right] = tmp;
+		print_array(arr, size);
+	}
+
+	return (i);
 }
